5f5ed4cbac0fa113c5c84cc7c99ed8a2c15f40ed
Thomas DA SILVA <thomas-78000@hotmail.fr>
2023-10-01 15:59:46 +0200

    rewrite_path() added on build()

--- srcs/requirements/source/class/HttpResponse/build.cpp
+++ srcs/requirements/source/class/HttpResponse/build.cpp
@@ -6,18 +6,20 @@
 /*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 11:00:08 by tda-silv          #+#    #+#             */
-/*   Updated: 2023/10/01 15:15:27 by tda-silv         ###   ########.fr       */
+/*   Updated: 2023/10/01 15:57:58 by tda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <header.hpp>
 
 static bool	is_allowed_methods( Location *location, std::string method );
+static void	rewrite_path( std::string &new_path, Location *location, std::string path );
 
 void	HttpResponse::build( Request &request, char **env, Server& server )	// ! throw possible
 {
 	Location					*location;
 	std::vector< std::string >	accepted_methods;
+	std::string					new_path;
 
 	if ( request.get_final_status() == bad_request )
 		my_perror_and_throw( "bad request", StatusCode( 400 ) );
@@ -27,9 +29,11 @@ void	HttpResponse::build( Request &request, char **env, Server& server )	// ! th
 		location->print_location();
 	else
 		std::cout << COLOR_BOLD_RED << "No matching location\n" << COLOR_RESET;
-
-
-
+	
+	
+	
+	rewrite_path( new_path, location, request.request_line.parsed_url.path );
+	//std::cout << COLOR_BOLD_GREEN << "============> " << new_path << COLOR_RESET << "\n";
 
 	if ( is_allowed_methods( location, request.request_line.method ) == true )
 	{
@@ -75,4 +79,27 @@ static bool	is_allowed_methods( Location *location, std::string method )
 	}
 
 	return ( false );
+}
+
+static void	rewrite_path( std::string &new_path, Location *location, std::string path )
+{
+	if ( !location)
+		new_path = path;
+	else
+	{
+		if ( location->_parameters.find( "root" ) != location->_parameters.end() )
+		{
+			if ( location->_parameters[ "root" ].empty() == false )
+			{
+				std::string	str;
+				
+				new_path = std::string( ROOT ) + "/" + location->_parameters[ "root" ][0];
+				str = path.substr( location->_uri.size() );
+				if (str.size() > 0 )
+					new_path += "/" + str;
+			}
+		}
+		else
+			new_path = path;
+	}
 }
