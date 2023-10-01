1d01267fb15317efb5425d6fc135c5b17756e5f1
yfoucade <yfoucade@student.42.fr>
2023-10-01 18:54:34 +0200

    handle case where path is a directory

--- srcs/requirements/source/class/HttpResponse/build.cpp
+++ srcs/requirements/source/class/HttpResponse/build.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   build.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 11:00:08 by tda-silv          #+#    #+#             */
-/*   Updated: 2023/10/01 16:51:04 by tda-silv         ###   ########.fr       */
+/*   Updated: 2023/10/01 17:53:31 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,44 @@
 
 static bool	is_allowed_methods( Location *location, std::string method );
 static void	rewrite_path( std::string &new_path, Location *location, std::string path );
+
+bool	is_directory( std::string name )
+{
+	DIR	*directory = opendir(name.c_str());
+	if ( !directory )
+		return false;
+	closedir(directory);
+	return true;
+}
+
+bool	is_in_directory( std::string dir, std::string file )
+{
+	DIR				*directory = opendir(dir.c_str());
+	struct dirent	*dir_entry;
+	bool			ret = false;
+
+	if ( !directory )
+		return false;
+	while ( (dir_entry = readdir(directory)) )
+	{
+		if ( file == dir_entry->d_name )
+		{
+			ret = true;
+			break;
+		}
+	}
+	closedir(directory);
+	return ret;
+}
+
+bool	autoindex_is_on( Location *location )
+{
+	if ( !location
+	|| location->_parameters.find("autoindex") == location->_parameters.end()
+	|| location->_parameters["autoindex"][0] != "on" )
+		return false;
+	return true;
+}
 
 void	HttpResponse::build( Request &request, char **env, Server& server )	// ! throw possible
 {
@@ -31,9 +69,18 @@ void	HttpResponse::build( Request &request, char **env, Server& server )	// ! th
 		std::cout << COLOR_BOLD_RED << "No matching location\n" << COLOR_RESET;
 	
 	
-	
+	new_path = server.root;
 	rewrite_path( new_path, location, request.request_line.parsed_url.path );
 	std::cout << COLOR_BOLD_GREEN << "path : " << new_path << COLOR_RESET << "\n";
+
+	if ( is_directory(new_path) )
+	{
+		if ( !autoindex_is_on(location) )
+			my_perror_and_throw( "Page not found", StatusCode( 404 ) );
+		else
+			// TODO: directory listing
+			;
+	}
 
 	if ( is_allowed_methods( location, request.request_line.method ) == true )
 	{
@@ -84,7 +131,7 @@ static bool	is_allowed_methods( Location *location, std::string method )
 static void	rewrite_path( std::string &new_path, Location *location, std::string path )
 {
 	if ( !location)
-		new_path = path;
+		new_path += path;
 	else
 	{
 		if ( location->_parameters.find( "root" ) != location->_parameters.end() )
@@ -93,13 +140,27 @@ static void	rewrite_path( std::string &new_path, Location *location, std::string
 			{
 				std::string	str;
 				
-				new_path = std::string( ROOT ) + "/" + location->_parameters[ "root" ][0];
+				new_path += location->_parameters[ "root" ][0];
 				str = path.substr( location->_uri.size() );
 				if (str.size() > 0 )
 					new_path += "/" + str;
 			}
 		}
 		else
-			new_path = path;
+			new_path += path;
+	}
+	
+	std::cout << "new_path: " << new_path << std::endl;
+	if ( is_directory(new_path) && ( location->_parameters.find( "index" ) != location->_parameters.end() ) )
+	{
+		std::vector< std::string >::iterator files_it = location->_parameters["index"].begin();
+		for ( ; files_it != location->_parameters["index"].end(); ++files_it )
+		{
+			if ( is_in_directory(new_path, *files_it) )
+			{
+				new_path += "/" + *files_it;
+				break;
+			}
+		}
 	}
 }
