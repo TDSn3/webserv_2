d914977864d1b2e359e275bea4832dab11b33d4e
Thomas Da Silva <thomas-78000@hotmail.fr>
2023-10-07 12:14:42 +0200

    write 100000000 to CGI OK

--- srcs/requirements/source/class/HttpResponse/build/_exec_cgi.cpp
+++ srcs/requirements/source/class/HttpResponse/build/_exec_cgi.cpp
@@ -6,46 +6,46 @@
 /*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 15:19:36 by tda-silv          #+#    #+#             */
-/*   Updated: 2023/10/05 10:00:16 by tda-silv         ###   ########.fr       */
+/*   Updated: 2023/10/07 11:55:21 by tda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <header.hpp>
 
+static void	check_file( std::string &path );
 static void	new_char_for_execve( Request &request, std::vector<char *> &arg_for_execve, std::string &path );
-static void	new_char_for_env_update( std::vector<char *> &env_update, char **env, Request &request );
 static void	env_update_push_back( std::vector<char *> &env_update, const char *str_to_add );
-static void	write_all_body( std::string &body, int stdin_pipefd[2] );
-static void fork_child( std::string &path, std::vector<char *> &arg_for_execve, std::vector<char *> &env_update, int stdout_pipefd[2], int stdin_pipefd[2] );
-static void fork_parent( Request &request, int pid, std::string &str,  int stdout_pipefd[2], int stdin_pipefd[2] );
+static void	new_char_for_env_update( std::vector<char *> &env_update, char **env, Request &request );
+static void	fork_child( int stdin_pipefd[2], int file_stock_output_fd, std::vector<char *> &arg_for_execve, std::vector<char *> &env_update );
+static void	fork_parent( int stdin_pipefd[2], std::string &str, int pid );
 
 std::string	HttpResponse::_exec_cgi( std::string &path, Request &request, char **env )	// ! throw possible
 {
-	struct stat			stat_buffer;
-	int					stdout_pipefd[2];
+	int					file_stock_output_fd;
 	int					stdin_pipefd[2];
 	pid_t				pid;
-	std::string			str;
+	std::string			file_stock_output_path(".stock.temp");
+	std::string			ret;
 	std::vector<char *>	arg_for_execve;
 	std::vector<char *>	env_update;
 
-	if ( stat( path.c_str(), &stat_buffer ) != 0 )		// Vérifie si le fichier existe
-		my_perror_and_throw( "cgi file does not exist", StatusCode( 404 ) );
-	if ( access( path.c_str(), R_OK | X_OK ) != 0 )		// Vérifie si le fichier est accessible en lecture et en exécution
-		my_perror_and_throw( "cgi file is not readable or executable", StatusCode( 403 ) );
+	file_stock_output_fd = open( file_stock_output_path.c_str(), O_RDWR | O_CREAT | O_TRUNC, 0666 );
+
+	check_file( path );	// ! throw possible
 
 	new_char_for_execve( request, arg_for_execve, path );
 	new_char_for_env_update( env_update, env, request );
 
-	pipe( stdout_pipefd );								// stdout_pipefd[0] en lecture, stdout_pipefd[1] en écriture
 	pipe( stdin_pipefd );
 
 	pid = fork();
 
 	if ( pid == 0 )
-		fork_child( path, arg_for_execve, env_update, stdout_pipefd, stdin_pipefd );
+		fork_child( stdin_pipefd, file_stock_output_fd, arg_for_execve, env_update );
 	else if (pid > 0)
-		fork_parent( request, pid, str, stdout_pipefd, stdin_pipefd );
+		fork_parent( stdin_pipefd, request.get_body(), pid );
+
+	close( file_stock_output_fd );
 
 	for ( size_t i = 0; i < arg_for_execve.size() -1 ; i++ )
 		delete [] arg_for_execve[i];
@@ -53,101 +53,17 @@ std::string	HttpResponse::_exec_cgi( std::string &path, Request &request, char *
 	for ( size_t i = 0; i < env_update.size() -1 ; i++ )
 		delete [] env_update[i];
 
-
-
-
-
-
-
-	// std::size_t pos = str.find("\n");
-	// if (pos != std::string::npos) { // Si \n a été trouvé
-	//     str.erase(0, pos + 1); // Supprimer la première ligne
-	// }
-
-
-
-
-
-
-// size_t i = 0;
-// size_t size = 0;
-// 	while (str[i])
-// 	{
-// 		size++;
-
-// 		if (str[i] == '\n')
-// 			size = 0;
-
-// 		i++;
-// 	}
-
-// 	std::ostringstream oss;
-// 	oss << size;
-
-	// str = "HTTP/1.1 200 OK\nContent-Length: " + oss.str() + "\n" + str;
-
-
-
-
-
-
-
-	// pos = str.find("\n");
-	// if (pos != std::string::npos) { // Si \n a été trouvé
-	//     str.erase(0, pos + 1); // Supprimer la première ligne
-	// }
-	// pos = str.find("\n");
-	// if (pos != std::string::npos) { // Si \n a été trouvé
-	//     str.erase(0, pos + 1); // Supprimer la première ligne
-	// }
-
-	// std::ostringstream oss;
-	// oss << str.size();
-
-	std::string sstr = std::string("HTTP/1.1 200 OK") + std::string("\nContent-Type: text/html; charset=utf-8\nContent-Length: 100000000\n\n");
-	for (size_t i = 0; i < 100000000 ; i++)
-		sstr += str.back();
-	// sstr = sstr + str;
-
-	return ( sstr );
+	return ( ret );
 }
 
-static void fork_child( std::string &path, std::vector<char *> &arg_for_execve, std::vector<char *> &env_update, int stdout_pipefd[2], int stdin_pipefd[2] )
+static void	check_file( std::string &path )				// ! throw possible
 {
-	close( stdout_pipefd[0] ); 					// ferme l'extrémité en lecture
-	close( stdin_pipefd[1] );
-
-	dup2( stdout_pipefd[1], STDOUT_FILENO );	// redirige stdout vers stdout_pipefd[1]
-	dup2( stdin_pipefd[0], STDIN_FILENO );
-
-	execve( path.c_str(), arg_for_execve.data(), env_update.data() );
-
-	exit( 1 );
-}
-
-static void fork_parent( Request &request, int pid, std::string &str,  int stdout_pipefd[2], int stdin_pipefd[2] )
-{
-	ssize_t	ret;
-	char	buffer[4096];
-
-	close( stdout_pipefd[1] );  				// ferme l'extrémité en écriture
-	close( stdin_pipefd[0] );
-
-	write_all_body( request.get_body(), stdin_pipefd );	// envois le body au CGI
-	close( stdin_pipefd[1] );
-
-	ret = 1;
-	while ( ret > 0 )
-	{
-		memset( buffer, 0, sizeof( buffer ) );
-		ret = read( stdout_pipefd[0], buffer, sizeof( buffer ) );
-		std::cout << COLOR_MAGENTA << ret << COLOR_RESET << "\n";
-		str += buffer;
-	}
-	std::cout << COLOR_MAGENTA << "size : " << str.size() << COLOR_RESET << "\n\n";
-	std::cout << COLOR_MAGENTA << str << COLOR_RESET << "\n";
-
-	waitpid( pid, NULL, 0 );					// TODO: ajouter une limite
+	struct stat			stat_buffer;
+	
+	if ( stat( path.c_str(), &stat_buffer ) != 0 )		// Vérifie si le fichier existe
+		my_perror_and_throw( "cgi file does not exist", StatusCode( 404 ) );
+	if ( access( path.c_str(), R_OK | X_OK ) != 0 )		// Vérifie si le fichier est accessible en lecture et en exécution
+		my_perror_and_throw( "cgi file is not readable or executable", StatusCode( 403 ) );
 }
 
 static void	new_char_for_execve( Request &request, std::vector<char *> &arg_for_execve, std::string &path )
@@ -166,36 +82,8 @@ static void	new_char_for_execve( Request &request, std::vector<char *> &arg_for_
 		std::strcpy( str, (it->first + "=" + it->second).c_str() );
 		arg_for_execve.push_back( str );
 	}
+
 	arg_for_execve.push_back( NULL );
-}
-
-static void	new_char_for_env_update( std::vector<char *> &env_update, char **env, Request &request )
-{
-	for ( size_t i = 0; env[i]; i++ )
-		env_update_push_back( env_update, env[i] );
-
-	env_update_push_back( env_update, "SERVER_SOFTWARE=Webserv/1.0" );
-	// env_update_push_back( env_update, "SERVER_NAME=localhost" );
-	env_update_push_back( env_update, "GATEWAY_INTERFACE=CGI/1.1" );
-	env_update_push_back( env_update, "SERVER_PROTOCOL=HTTP/1.1" );
-	env_update_push_back( env_update, "SERVER_PORT=8080" );
-	// env_update_push_back( env_update, "SCRIPT_NAME=/directory/youpi.bla" );
-	// env_update_push_back( env_update, "SCRIPT_FILENAME=/Users/thomas/Desktop/42/webserv_2/srcs/requirements/cgi_tester" );
-	env_update_push_back( env_update, "REDIRECT_STATUS=200" );
-
-
-	env_update_push_back( env_update, ( "REQUEST_METHOD=" + request.request_line.method ).c_str() );
-	
-	env_update_push_back( env_update,  "PATH_INFO=/" );
-	// env_update_push_back( env_update,  "PATH_INFO=/Users/thomas/Desktop/42/webserv_2/srcs/requirements/cgi_tester" );
-	
-	env_update_push_back( env_update,  "CONTENT_TYPE=application/x-www-form-urlencoded" );
-	
-	env_update_push_back( env_update,  "QUERY_STRING=" );
-
-	env_update_push_back( env_update, "CONTENT_LENGTH=100000000" );
-
-	env_update.push_back( NULL );
 }
 
 static void	env_update_push_back( std::vector<char *> &env_update, const char *str_to_add )
@@ -207,53 +95,47 @@ static void	env_update_push_back( std::vector<char *> &env_update, const char *s
 	env_update.push_back( str );
 }
 
-static void	write_all_body( std::string &body, int stdin_pipefd[2] )
+static void	new_char_for_env_update( std::vector<char *> &env_update, char **env, Request &request )
 {
-	// ssize_t written;
-	// ssize_t	total_written;
-	// ssize_t size_body;
+	(void) request;
 
-	// total_written = 0;
-	// size_body = body.size();
-	// int i = 0;
-	// while ( total_written < size_body )
-	// {
-	// 	std::cout << COLOR_RED << i++ << COLOR_RESET<< "\n";
-	// 	written = write( stdin_pipefd[1], body.c_str(), 65536 );	// 64 ko = 65536
-	// 	std::cout << COLOR_RED << "written : " << written << COLOR_RESET<< "\n";
-	// 	std::cout << COLOR_RED << "body size : " << body.size() << COLOR_RESET<< "\n";
-	// 	total_written += written;
-	// 	body.erase( 0, written );
-	// }
-	// std::cout << COLOR_RED << "end" << COLOR_RESET<< "\n";
+	for ( size_t i = 0; env[i]; i++ )
+		env_update_push_back( env_update, env[i] );
 
-	// write( stdin_pipefd[1], body.c_str(), 100 );	// 64 ko = 65536
+	env_update_push_back( env_update, "AUTH_TYPE=None" );
+	env_update_push_back( env_update, "CONTENT_LENGTH=100000000" );
+	env_update_push_back( env_update, "CONTENT_TYPE=test/file" );
+	env_update_push_back( env_update, "GATEWAY_INTERFACE=None" );
+	env_update_push_back( env_update, "PATH_INFO=YoupiBanane/youpi.bla" );
+	env_update_push_back( env_update, "PATH_TRANSLATED=YoupiBanane/youpi.bla" );
+	env_update_push_back( env_update, "QUERY_STRING=" );
+	env_update_push_back( env_update, "REMOTE_ADDR=16777343" );
+	env_update_push_back( env_update, "REMOTE_INDENT=None" );
+	env_update_push_back( env_update, "REMOTE_USER=localhost:8080" );
+	env_update_push_back( env_update, "REQUEST_METHOD=POST" );
+	env_update_push_back( env_update, "REQUEST_URI=YoupiBanane/youpi.bla" );
+	env_update_push_back( env_update, "SCRIPT_NAME=cgi/cgi_tester" );
+	env_update_push_back( env_update, "SERVER_NAME=127.0.0.1" );
+	env_update_push_back( env_update, "SERVER_PORT=8080" );
+	env_update_push_back( env_update, "SERVER_PROTOCOL=HTTP/1.1" );
+	env_update_push_back( env_update, "SERVER_SOFTWARE=None" );
 
-	size_t	to_write;
-	ssize_t	written;
-	size_t	total_written;
+	env_update.push_back( NULL );
+}
 
-	to_write = body.size();
-	written = 0;
-	total_written = 0;
 
-	std::cout << COLOR_BOLD_YELLOW << "body size : " << body.size() << std::endl;
-	std::cout << COLOR_BOLD_YELLOW << "written : " << written << std::endl;
-	std::cout << COLOR_BOLD_YELLOW << "total_written : " << total_written << std::endl;
-	std::cout << COLOR_BOLD_YELLOW << "to_write : " << to_write << "\n" << std::endl;
+static void fork_child( int stdin_pipefd[2], int file_stock_output_fd, std::vector<char *> &arg_for_execve, std::vector<char *> &env_update )
+{
+	dup2( stdin_pipefd[0], STDIN_FILENO );
+	close( stdin_pipefd[1] );
+	dup2( file_stock_output_fd, STDOUT_FILENO );
+	execve( arg_for_execve[0], arg_for_execve.data(), env_update.data() );
+}
 
-	while ( to_write != 0 )
-	{
-		fcntl( stdin_pipefd[1], F_SETFL, O_NONBLOCK );
-		written = write( stdin_pipefd[1], body.c_str() + total_written , to_write );
-		if (written == -1)
-			return ;
-
-		total_written += written;
-   		to_write -= written;
-
-		std::cout << COLOR_BOLD_YELLOW << "written : " << written << std::endl;
-		std::cout << COLOR_BOLD_YELLOW << "total_written : " << total_written << std::endl;
-		std::cout << COLOR_BOLD_YELLOW << "to_write : " << to_write << "\n" << std::endl;
-	}
+static void fork_parent( int stdin_pipefd[2], std::string &str, int pid )
+{
+	close( stdin_pipefd[0] );
+	write( stdin_pipefd[1], str.c_str(), str.size() );
+	close( stdin_pipefd[1] );
+	waitpid(pid, 0, 0);
 }
