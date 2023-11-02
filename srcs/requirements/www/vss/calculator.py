f245030142de21f6f114c05a4a9458c4f9fd90e5
Yohan Foucade <yohan.foucade@gmail.com>
2023-11-02 14:08:03 +0100

    create test website

---
+++ srcs/requirements/www/vss/calculator.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+import cgi
+import sys
+
+content = "Script CGI en Python :"
+
+for arg in sys.argv:
+	content += "\n" + arg
+
+content_length = len(content.encode('utf-8'))
+
+
+
+print("HTTP/1.1 200 OK")
+
+print("content-type: text/html")
+print(f"Content-Length: {content_length}")
+print()
+
+print(content)
