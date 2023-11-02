8f688150bc1bd1589271a6580db8dcc05cae5ef0
Yohan Foucade <yohan.foucade@gmail.com>
2023-11-02 13:56:47 +0100

    handle failure, and use class CGIResponse

--- srcs/requirements/source/class/HttpResponse/build/_exec_cgi.cpp
+++ srcs/requirements/source/class/HttpResponse/build/_exec_cgi.cpp
@@ -6,7 +6,7 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 15:19:36 by tda-silv          #+#    #+#             */
-/*   Updated: 2023/11/01 23:13:23 by yfoucade         ###   ########.fr       */
+/*   Updated: 2023/11/02 13:39:36 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,11 @@ static void			new_char_for_env_update( std::vector<char *> &env_update, char **e
 static void			fork_child( int stdin_pipefd[2], int file_stock_output_fd, std::vector<char *> &arg_for_execve, std::vector<char *> &env_update );
 static void			fork_parent( int stdin_pipefd[2], std::string &str, int pid );
 static void			read_file_stock_output( int file_stock_output_fd, std::string &str );
-static std::string	to_lower_str( std::string str );
+// static std::string	to_lower_str( std::string str );
 static std::string	to_upper_str( std::string str );
 static std::string	dash_to_underscore( std::string str );
-static void			parse_cgi_output( std::string &str, std::string &body );
+// static void			parse_cgi_output( std::string &str, std::string &body );
+static void			parse_cgi_output2( std::string &str, std::string &cgi_output );
 
 std::string	HttpResponse::_exec_cgi( std::string &path, std::string &path_target, Request &request, char **env, Server &server )	// ! throw possible
 {
@@ -31,6 +32,7 @@ std::string	HttpResponse::_exec_cgi( std::string &path, std::string &path_target
 	pid_t				pid;
 	std::string			file_stock_output_path(".TEMP");
 	std::string			ret;
+	std::string			cgi_output;
 	std::vector<char *>	arg_for_execve;
 	std::vector<char *>	env_update;
 
@@ -39,39 +41,50 @@ std::string	HttpResponse::_exec_cgi( std::string &path, std::string &path_target
 	// path_target = path_target.substr( server.root.size() + 1 );
 	// std::cout << "============> " << path_target << "\n";
 
-	// TODO: check failure of open
 	file_stock_output_fd = open( file_stock_output_path.c_str(), O_RDWR | O_CREAT | O_TRUNC, 0666 );
 
-	check_file( path );	// ! throw possible
+	try
+	{
+		check_file( path );	// ! throw possible
 
-	// TODO: handle failure of new[]
-	new_char_for_execve( request, arg_for_execve, path );
-	new_char_for_env_update( env_update, env, request, path, path_target, server );
+		new_char_for_execve( request, arg_for_execve, path );
+		new_char_for_env_update( env_update, env, request, path, path_target, server );
 
-	pipe( stdin_pipefd );
+		pipe( stdin_pipefd );
 
-	pid = fork();
+		pid = fork();
 
-	if ( pid == 0 )
-		fork_child( stdin_pipefd, file_stock_output_fd, arg_for_execve, env_update );
-	else if (pid > 0)
-		fork_parent( stdin_pipefd, request.get_body(), pid ); // TODO: if FAILURE, close file_stock_output_fd
+		if ( pid == 0 )
+			fork_child( stdin_pipefd, file_stock_output_fd, arg_for_execve, env_update );
+		else if (pid > 0)
+			fork_parent( stdin_pipefd, request.get_body(), pid );
 
-	// TODO: if not failure of fork_parent, do:
-	read_file_stock_output( file_stock_output_fd, ret );
-	// TODO: delete vectors content (see below),
-	// then if FAILURE of read_file_stock_output, set 'ret' to empty string and return
+		read_file_stock_output( file_stock_output_fd, cgi_output );
 
-	for ( size_t i = 0; i < arg_for_execve.size() -1 ; i++ )
-		delete [] arg_for_execve[i];
+		for ( size_t i = 0; i < arg_for_execve.size() -1 ; i++ )
+			delete [] arg_for_execve[i];
 
-	for ( size_t i = 0; i < env_update.size() -1 ; i++ )
-		delete [] env_update[i];
+		for ( size_t i = 0; i < env_update.size() -1 ; i++ )
+			delete [] env_update[i];
 
-	parse_cgi_output( ret, request.get_body() );
+		// parse_cgi_output( ret, request.get_body() );
+		parse_cgi_output2( ret, cgi_output );
 
-	return ( ret );
+		return ( ret );
 
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+		
+		for ( size_t i = 0; i < arg_for_execve.size(); i++ )
+			delete [] arg_for_execve[i];
+
+		for ( size_t i = 0; i < env_update.size(); i++ )
+			delete [] env_update[i];
+
+		throw;
+	}
 }
 
 static void	check_file( std::string &path )				// ! throw possible
@@ -179,9 +192,9 @@ static void fork_child( int stdin_pipefd[2], int file_stock_output_fd, std::vect
 static void fork_parent( int stdin_pipefd[2], std::string &str, int pid )
 {
 	close( stdin_pipefd[0] );
-	write( stdin_pipefd[1], str.c_str(), str.size() ); // TODO: cannot write without going through poll. Use regular file
+	write( stdin_pipefd[1], str.c_str(), str.size() );
 	close( stdin_pipefd[1] );
-	waitpid(pid, 0, 0); // TODO: add timeout and if timeout kill and return FAILURE 
+	waitpid(pid, 0, 0);
 }
 
 static void	read_file_stock_output( int file_stock_output_fd, std::string &str )
@@ -191,35 +204,30 @@ static void	read_file_stock_output( int file_stock_output_fd, std::string &str )
 
 	ret = 1;
 	lseek( file_stock_output_fd, 0, SEEK_SET );	// Réinitialise le pointeur du fd
+
+	ret = read( file_stock_output_fd, buffer, sizeof( buffer ) );
 	while ( ret > 0 )
 	{
-		ret = read( file_stock_output_fd, buffer, sizeof( buffer ) );
 		str.append( buffer, ret );
+		ret = read( file_stock_output_fd, buffer, sizeof( buffer ) );
 	}
-
-	// TODO
-	// ret = read( file_stock_output_fd, buffer, sizeof( buffer ) );
-	// while ( ret > 0 )
-	// {
-	// 	str.append( buffer, ret );
-	// 	ret = read( file_stock_output_fd, buffer, sizeof( buffer ) );
-	// }
-	// close( file_stock_output_fd );
-	// if ( ret == -1 )
-	// 	return ( FAILURE );
-	// return ( SUCCESS );
+	close( file_stock_output_fd );
+	if ( ret == -1 )
+	{
+		my_perror_and_throw( "Internal Server Error", 500 );
+	}
 
 	close( file_stock_output_fd );
 }
 
-static std::string to_lower_str( std::string str )
-{
-	std::string	ret;
+// static std::string to_lower_str( std::string str )
+// {
+// 	std::string	ret;
 
-	for ( size_t i = 0; str[i]; i++ )
-		ret += std::tolower( static_cast<unsigned char> ( str[i] ) ) ;
-	return ( ret );
-}
+// 	for ( size_t i = 0; str[i]; i++ )
+// 		ret += std::tolower( static_cast<unsigned char> ( str[i] ) ) ;
+// 	return ( ret );
+// }
 
 static std::string to_upper_str( std::string str )
 {
@@ -239,29 +247,36 @@ static std::string dash_to_underscore( std::string str )
 	return ( ret );
 }
 
-static void	parse_cgi_output( std::string &str, std::string &body )
-{
-	std::string			header_update;
-	std::ostringstream	oss;
+// static void	parse_cgi_output( std::string &str, std::string &body )
+// {
+// 	// TODO: create a CGIResponse object that will construct the response
+// 	// from the CGI output.
+// 	std::string			header_update;
+// 	std::ostringstream	oss;
 
-	size_t	start = 0;
-	for ( size_t i = 0; str[i]; i++ )
-	{
-		if ( str[i] == '\n' && str[i + 1] && ( str[i + 1] == '\n' || ( str[i + 1] == '\r' && str[i + 2] && str[i + 2] == '\n' ) ) )
-		{
-			oss << body.size();
-			str.insert( i + 1, "Content-Length: " + oss.str() + "\r\n" );
-			return ;
-		}
-		if ( i > 0 && str[i - 1] == '\n' )
-			start = i;
-		if ( str[i] == '\n' && i != start )	// Si je suis à la fin de la ligne et que le début de la ligne n'est pas un '\n'
-		{
-			if ( to_lower_str( str.substr( start, start + 7) ) == "status:" )
-			{
-				str.replace( start, start + 7, "HTTP/1.1" );
-				i++;
-			}
-		}
-	}
+// 	size_t	start = 0;
+// 	for ( size_t i = 0; str[i]; i++ )
+// 	{
+// 		if ( str[i] == '\n' && str[i + 1] && ( str[i + 1] == '\n' || ( str[i + 1] == '\r' && str[i + 2] && str[i + 2] == '\n' ) ) )
+// 		{
+// 			oss << body.size();
+// 			str.insert( i + 1, "Content-Length: " + oss.str() + "\r\n" );
+// 			return ;
+// 		}
+// 		if ( i > 0 && str[i - 1] == '\n' )
+// 			start = i;
+// 		if ( str[i] == '\n' && i != start )	// Si je suis à la fin de la ligne et que le début de la ligne n'est pas un '\n'
+// 		{
+// 			if ( to_lower_str( str.substr( start, start + 7) ) == "status:" )
+// 			{
+// 				str.replace( start, start + 7, "HTTP/1.1" );
+// 				i++;
+// 			}
+// 		}
+// 	}
+// }
+
+static void	parse_cgi_output2( std::string &str, std::string &cgi_output )
+{
+	str = CGIResponse( cgi_output ).parsed_response;
 }
